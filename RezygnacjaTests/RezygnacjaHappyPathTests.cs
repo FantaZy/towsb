b077a0e2cfe2fa434cab1f2bd73061ab6392e7fe
Maciek <brayant@op.pl>
2014-11-28 23:21:52 +0100

    second commit

---
+++ RezygnacjaTests/RezygnacjaHappyPathTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RezygnacjaTests
+{
+    [TestClass]
+    public class RezygnacjaHappyPathTests
+    {
+        [TestMethod]
+        public void TestHappyPath_ShowEventsForUser()
+        {
+            //GIVEN
+            var user = new User("user1");
+            DBAdapter adapter = new MockDB();
+            var gig = new EventGig("Jazz Concert", 50);
+            adapter.RecordData(gig, user);
+            
+            var eventManager = new EventManager(adapter);
+            
+            //WHEN
+            var events = eventManager.GetEventsForUser(user);
+            //THEN
+            Assert.AreEqual(1, events.Count);
+            Assert.AreEqual(gig, events[0]);
+        }
+
+        [TestMethod]
+        public void TestHappyPath_WhenUserResignedFromEventThisEventIsNoLongerInHisEventsList()
+        {
+            //GIVEN
+            var user = new User("user1");
+            DBAdapter adapter = new MockDB();
+            adapter.RecordData(new EventGig("Jazz Concert", 50), user);
+
+            var eventManager = new EventManager(adapter);
+            //WHEN
+            var events = eventManager.GetEventsForUser(user);
+            eventManager.ResignFromEventForUser(user, events[0]);
+
+            var updatedEvents = eventsManager.GetEventsForUser(user);
+
+            //THEN
+            Assert.AreEqual(0, updatedEvents.Count);
+            
+        }
+
+        [TestMethod]
+        public void TestHappyPath_WhenUserResignedFromEventSuccessfuullyMoneyIsReturnedToUsersAccount()
+        {
+            //GIVEN
+            var user = new User("user1");
+            DBAdapter adapter = new MockDB();
+            adapter.RecordData(new EventGig("Jazz Concert", 50), user);
+
+            var eventManager = new EventManager(adapter);
+            //WHEN
+            var events = eventManager.GetEventsForUser(user);
+            eventManager.ResignFromEventForUser(user, events[0]);
+
+            
+
+            //THEN
+            Assert.AreEqual(50, user.Account.Value);
+
+        }
+    }
+}
---
+++ RezygnacjaTests/RezygnacjaUnhappyPathTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RezygnacjaTests
+{
+    [TestClass]
+    public class RezygnacjaUnhappyPathTests
+    {
+        [TestMethod]
+        public void RezygnacjaUnhappyPathTest_WhenUserResignsFromEventThatIsNotOnHisEventsListAErrorMessageIsAddedToMessageWindowAndAccountValueIsNotModified()
+        {
+            //GIVEN
+            var user = new User("user1");
+            DBAdapter adapter = new MockDB();
+            adapter.RecordData(new EventGig("Jazz Concert", 50), user);
+
+            var eventManager = new EventManager(adapter);
+            var nonExistantEvent = new EventGig("Folk Concert", 125);
+            int eventsCount = eventManager.GetEventsForUser(user).Count;
+            int money = user.Account.Value;
+            //WHEN
+            eventManager.ResignFromEventForUser(user, nonExistantEvent);
+
+            //THEN
+            Assert.AreEqual(eventsCount, eventManager.GetEventsForUser(user).Count);
+            Assert.AreEqual(money, user.Account.Value);
+        }
+    }
+}
