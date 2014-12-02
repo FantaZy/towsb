299a63427b0f07bdba9235b1b56ceaa3afae161a
china92 <china92@github.com>
2014-12-02 21:53:49 +0100

    come initial configuration and classes added

---
+++ RezygnacjaTests/MockDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rezygnacja;
+
+namespace RezygnacjaTests
+{
+    class MockDB : DBAdapter
+    {
+    }
+}
